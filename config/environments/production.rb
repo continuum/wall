7fba3d065d7fa5481cf2ced2e1b7723ba66e93a6
Leonardo Soto <leo.soto@gmail.com>
2013-03-17 17:09:56 -0300

    Config de SendGrid para heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,15 @@ Wall::Application.configure do
     :entitystore  => 'file:tmp/cache/rack/body',
     :allow_reload => false
   }
+
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
